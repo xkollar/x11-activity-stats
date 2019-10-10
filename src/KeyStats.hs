17abb50bcbd24a32d770957f4935665f9d8c4354
Matej Kollar <208115@mail.muni.cz>
2019-10-10 19:24:29 +0100

    We handle new windows

--- src/KeyStats.hs
+++ src/KeyStats.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NamedFieldPuns #-}
 module KeyStats
     ( main
     , fib
@@ -6,14 +8,17 @@ module KeyStats
 
 import Prelude (Integer, (+), (-), (*), (^), abs)
 
+import Control.Applicative (pure)
 import Control.Concurrent (MVar, newMVar)
 import Control.Exception (bracket)
-import Control.Monad ((>>=), (>>), forever, mapM_)
+import Control.Monad ((>>=), (>>), forever, when)
 import Data.Bits ((.|.))
-import Data.Bool (otherwise)
-import Data.Function (($))
+import Data.Bool (Bool(False), otherwise)
+import Data.Eq ((==))
+import Data.Foldable (for_)
+import Data.Function (($), (.))
 import Data.Ord ((<))
-import System.IO (IO, print)
+import System.IO (IO, print, putStr, putStrLn)
 
 import Graphics.X11.Xlib
     ( Display
@@ -22,15 +27,28 @@ import Graphics.X11.Xlib
     , allocaXEvent
     , asKeyEvent
     , closeDisplay
+    , createNotify
+    , keyPress
     , defaultRootWindow
+    , enterWindowMask
     , keyPressMask
     , lookupString
     , nextEvent
     , openDisplay
     , pointerMotionMask
+    , propertyChangeMask
     , selectInput
+    , substructureNotifyMask
+    , sync
     )
-import Graphics.X11.Xlib.Extras (getEvent, queryTree)
+import Graphics.X11.Xlib.Extras
+    ( Event(AnyEvent, KeyEvent)
+    , ev_event_type
+    , ev_window
+    , getEvent
+    , none
+    , queryTree
+    )
 import qualified Data.Map.Strict as M
 
 
@@ -42,22 +60,29 @@ fib m | m < 0     = (-1 ^ abs m) * fib (abs m)
     go 1 = 1
     go n = go (n-1) + go (n-2)
 
-selectTree :: Display -> Window -> EventMask -> IO ()
-selectTree d w m = do
-    selectInput d w m
-    (_,_,ws) <- queryTree d w
-    mapM_ (\x -> selectTree d x m) ws
+selectTree :: EventMask -> Display -> Window -> IO ()
+selectTree eventMask display = go
+  where
+    go w = do
+        (_root, _parent, children) <- queryTree display w
+        selectInput display w eventMask
+        for_ children go
+
+setAllEvents :: Display -> Window -> IO ()
+setAllEvents = selectTree (substructureNotifyMask .|. keyPressMask)
 
 main :: IO ()
 main = do
     bracket (openDisplay "") closeDisplay $ \ display -> do
         print display
-        let root = defaultRootWindow display
-        print root
-        selectTree display root keyPressMask -- $ keyPressMask .|. buttonReleaseMask
-        selectTree display root $ keyPressMask .|. pointerMotionMask
-        allocaXEvent $ \ e -> do
-            forever $ do
-                nextEvent display e
-                getEvent e >>= print
-                -- lookupString (asKeyEvent e) >>= print
+        setAllEvents display (defaultRootWindow display)
+        sync display False
+        allocaXEvent $ \ e -> forever $ do
+            nextEvent display e
+            getEvent e >>= \case
+                ev@AnyEvent{ev_event_type, ev_window} -> do
+                    putStr "Any: " >> print ev
+                    when (ev_event_type == createNotify) $ setAllEvents display ev_window
+                KeyEvent{ev_event_type} -> do
+                    when (ev_event_type == keyPress) $ do lookupString (asKeyEvent e) >>= print
+                ev -> pure ()
