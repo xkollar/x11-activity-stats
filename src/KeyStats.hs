145172217843786d520a621485bd7bb0a208effc
Matej Kollar <208115@mail.muni.cz>
2019-10-10 17:18:31 +0100

    Working example

--- src/KeyStats.hs
+++ src/KeyStats.hs
@@ -6,10 +6,32 @@ module KeyStats
 
 import Prelude (Integer, (+), (-), (*), (^), abs)
 
+import Control.Concurrent (MVar, newMVar)
+import Control.Exception (bracket)
+import Control.Monad ((>>=), (>>), forever, mapM_)
+import Data.Bits ((.|.))
 import Data.Bool (otherwise)
+import Data.Function (($))
 import Data.Ord ((<))
-
 import System.IO (IO, print)
+
+import Graphics.X11.Xlib
+    ( Display
+    , EventMask
+    , Window
+    , allocaXEvent
+    , asKeyEvent
+    , closeDisplay
+    , defaultRootWindow
+    , keyPressMask
+    , lookupString
+    , nextEvent
+    , openDisplay
+    , pointerMotionMask
+    , selectInput
+    )
+import Graphics.X11.Xlib.Extras (getEvent, queryTree)
+import qualified Data.Map.Strict as M
 
 
 fib :: Integer -> Integer
@@ -20,5 +42,22 @@ fib m | m < 0     = (-1 ^ abs m) * fib (abs m)
     go 1 = 1
     go n = go (n-1) + go (n-2)
 
+selectTree :: Display -> Window -> EventMask -> IO ()
+selectTree d w m = do
+    selectInput d w m
+    (_,_,ws) <- queryTree d w
+    mapM_ (\x -> selectTree d x m) ws
+
 main :: IO ()
-main = print ()
+main = do
+    bracket (openDisplay "") closeDisplay $ \ display -> do
+        print display
+        let root = defaultRootWindow display
+        print root
+        selectTree display root keyPressMask -- $ keyPressMask .|. buttonReleaseMask
+        selectTree display root $ keyPressMask .|. pointerMotionMask
+        allocaXEvent $ \ e -> do
+            forever $ do
+                nextEvent display e
+                getEvent e >>= print
+                lookupString (asKeyEvent e) >>= print
